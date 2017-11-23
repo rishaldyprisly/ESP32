950ea6606f990ac0e5f952ee57cfc40d70670434
Rishaldy Prisly <aldylogy@gmail.com>
2017-11-23 10:43:07 +0700

    Update ESP32DevBoardMQTTClientSub.ino

--- ESP32DevBoardMQTTClientSub.ino
+++ ESP32DevBoardMQTTClientSub.ino
@@ -31,7 +31,7 @@ void setup_wifi() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
